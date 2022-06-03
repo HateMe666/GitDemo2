44c3477736ed6bb7dfe00b76a0d628e146dfd2e0
HateMe666 <nikita.kuzmich.11@gmail.com>
2022-06-03 13:43:59 +0300

    Init commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Nikita Kuzmich on 3.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
